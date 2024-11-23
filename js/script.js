08f618f9633703cf9abfd663d8d116f6e26565e0
PinProjectTEAM <pinprojectteam@gmail.com>
2024-11-24 00:12:12 +0500

    Update script.js
    remove old logo change

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleSwitch = document.getElementById('theme-toggle');
-    const logo = document.getElementById('logo');
 
     const currentTheme = localStorage.getItem('theme');
     if (currentTheme) {
         document.body.classList.add(currentTheme);
         if (currentTheme === 'dark') {
             toggleSwitch.checked = true;
-            logo.src = 'images/pinproject_dark.jpg';
         }
     }
 
@@ -15,11 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
         if (this.checked) {
             document.body.classList.add('dark');
             localStorage.setItem('theme', 'dark');
-            logo.src = 'images/pinproject_dark.jpg';
         } else {
             document.body.classList.remove('dark');
             localStorage.setItem('theme', 'light');
-            logo.src = 'images/pinproject.jpg';
         }
     });
 });
